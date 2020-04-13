f0e255a645d210049105e706fc9a762e2f98a35c
Nic Patterson <224374+reynn@users.noreply.github.com>
2020-04-13 16:44:16 -0700

    add clap opts for configurable config file

--- src/bin/grabber/main.rs
+++ src/bin/grabber/main.rs
@@ -1,7 +1,17 @@
+use clap::Clap;
 use grabber::config::AppConfig;
 
+#[derive(Debug, Clone, Default, Clap)]
+#[clap(version = "0.1.0", author = "Reynn")]
+struct Opts {
+    #[clap(short = "c", long = "config", default_value = ".grabber.toml")]
+    config: String,
+}
+
 fn main() {
-    let config: AppConfig = AppConfig::new(".grabber.toml").unwrap_or_else(|err| {
+    let opts: Opts = Opts::parse();
+
+    let config: AppConfig = AppConfig::new(opts.config.as_str()).unwrap_or_else(|err| {
         eprintln!("Failed to create app configuration [{}]", err);
         std::process::exit(2);
     });
